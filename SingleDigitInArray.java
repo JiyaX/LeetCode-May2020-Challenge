a535a321af9c110f061a5772ad5363318c707e65
Jiya <39413441+JiyaX@users.noreply.github.com>
2020-05-13 22:08:12 +0530

    Create SingleDigitInArray.java

---
+++ SingleDigitInArray.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int singleNonDuplicate(int[] nums) {
+      int n = nums.length;
+       
+        
+        for (int i=0,j=1;i<n-1;i+=2){
+            if (nums[i]!=nums[j]){
+                return nums[i];
+            }
+            j+=2;
+            
+             if (n == 1)
+             {return nums[1];}
+        }
+        return nums[n-1];
+        
+    }
+}
+    
